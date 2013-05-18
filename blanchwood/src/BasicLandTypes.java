51758d2aff83700d605b47fb66261c0aabf2e43c
sbamberg <sbamberg@students.uni-mainz.de>
2013-05-19 01:18:50 +0200

    Just the first thought in a sourcecode

---
+++ blanchwood/src/BasicLandTypes.java
@@ -0,0 +1,3 @@
+public enum BasicLandTypes {
+	PLAINS, ISLAND, SWAMP, MOUNTAIN, FOREST;
+}
---
+++ test/src/parse.java
@@ -0,0 +1,20 @@
+public class parse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	    int i = 456;
+	    Integer j = new Integer(789);
+	    String string = String.valueOf(i);
+	    System.out.println("string: " + string);
+	    string = j.toString();
+	    System.out.println("string: " + string);
+	    System.out.println("i und j direkt: " + i + " " + j);		
+			
+	}
+
+}
